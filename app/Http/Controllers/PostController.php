881f9320055fdff2171c8f496dde4e2ca578f1aa
Dinko <dinko.dugec@gmail.com>
2022-12-10 22:50:40 +0100

    simple route for one post from show method in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-   public function show()
+   public function show($id)
    {
+
+   /*    dd($id); */
+
+      Post::findOrFail($id);
 
      return view('blog-post');
 
