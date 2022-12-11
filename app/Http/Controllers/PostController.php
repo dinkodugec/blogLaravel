3e38eb1d7c9499b7ae518bd7504704900afac659
Dinko <dinko.dugec@gmail.com>
2022-12-11 22:14:34 +0100

    displaying a post from admin

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -48,7 +48,9 @@ class PostController extends Controller
    public function index()
    {
 
-    return view('admin.posts.index');
+     $posts = Post::all();
+
+    return view('admin.posts.index', ['posts' => $posts]);
 
    }
 }
