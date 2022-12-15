b38b5d9e81790689cf091df28926355503f7d728
Dinko <dinko.dugec@gmail.com>
2022-12-15 22:12:16 +0100

    setting relations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-           
+
                factory(App\User::class, 50)->create()->each(function ($user) {
                 $user->posts()->save(factory(App\Post::class)->make());
             });
