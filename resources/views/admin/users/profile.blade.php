2730234c00f863b7e0f6a730313eefc8f5ba14bf
Dinko <dinko.dugec@gmail.com>
2022-12-16 23:31:28 +0100

    updating user profile

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -6,6 +6,67 @@
 
   <h1 class="h3 mb-4 text-gray-800">Profile for {{ $user->name }}</h1>
 
+  <div class="row">
+    <div class="col-sm-6">
+
+
+      <form action="" method="post" enctype="multipart/form-data">
+        @csrf
+
+        <div class="mb-4">
+          <img class="img-profile rounded-circle" src="https://dummyimage.com/300x300/000/fff" alt="">
+        </div>
+
+       <div class="form-group">
+        <input type="file">
+       </div>
+
+        <div class="form-group">
+          <label for="name">NAme</label>
+             <input type="text"
+                     name="name"
+                     class="form-control"
+                     id="name"
+                     value="{{ $user->name }}">
+        </div>
+
+        <div class="form-group">
+          <label for="email">Email</label>
+             <input type="text"
+                     name="email"
+                     class="form-control"
+                     id="email"
+                     value="{{ $user->email }}">
+        </div>
+
+
+        <div class="form-group">
+          <label for="password">Password</label>
+             <input type="password"
+                     name="password"
+                     class="form-control"
+                     id="password"
+                     >
+        </div>
+
+        <div class="form-group">
+          <label for="password_confirmation">Confirm Password</label>
+             <input type="password_confirmation"
+                     name="password_confirmation"
+                     class="form-control"
+                     id="password_confirmation"
+                     >
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+       </form>
+
+
+    </div>
+  </div>
+
 
 
 
--- resources/views/components/admin-top-navbar-user-information.blade.php
+++ resources/views/components/admin-top-navbar-user-information.blade.php
@@ -13,7 +13,7 @@
   </a>
   <!-- Dropdown - User Information -->
   <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-    <a class="dropdown-item" href="#">
+    <a class="dropdown-item" href="{{ route('user.profile.show', auth()->user()) }}">
       <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
       Profile
     </a>
@@ -52,7 +52,7 @@
 
         </form>
         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-        
+
       </div>
     </div>
   </div>
